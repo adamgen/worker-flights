5bea7d8f0e67715cfe80e3c72e3c5b69f37ec623
Adam <adamgensh@gmail.com>
2023-05-22 15:23:21 +0300

    Add proxy to dev serve

---
+++ proxy.conf.js
@@ -0,0 +1,8 @@
+const defaultTarget = "http://20.66.25.216:3000/";
+
+module.exports = {
+  "*": {
+    target: defaultTarget,
+    secure: false,
+  },
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
-import { MockHttpService } from './mock-http.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, MatListModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    MatListModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  workers = this.mockHttpService.get('workers');
+  workers = this.httpClient.get<{ id: number; name: string }[]>('/workers');
 
-  constructor(public mockHttpService: MockHttpService) {}
+  constructor(private httpClient: HttpClient) {}
 }
